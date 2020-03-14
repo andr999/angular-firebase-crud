8e03dc593c2d5053f86c9720e5aadfbb1f231876
andr999 <andreykuchay2@gmail.com>
2020-03-14 22:08:06 +0200

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAbaTFEhywswU26rkKzcfE08Po88NnZXlQ",
+    authDomain: "fir-angular-auth-180ef.firebaseapp.com",
+    databaseURL: "https://fir-angular-auth-180ef.firebaseio.com",
+    projectId: "fir-angular-auth-180ef",
+    storageBucket: "fir-angular-auth-180ef.appspot.com",
+    messagingSenderId: "806062886174",
+    appId: "1:806062886174:web:33004fb3adc7fc59f5cd0f"
+
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
